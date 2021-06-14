7a467a243ba78dd7f3a9c9315c4359f738b6855e
Vertika <mittalvertika1803200@gmail.com>
2021-06-14 18:43:37 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const app=express();
+const ejs=require("ejs")
+const bodyParser=require("body-parser");
+const port=process.env.PORT||3000;
+const axios = require("axios");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+app.get("/about",(req,res)=>{
+    res.send("about");
+})
+app.get("/weather",(req,res)=>{
+    res.send("weather");
+})
+app.get("*",(req,res)=>{
+    res.send("404 error");
+})
+app.listen(port,(req,res)=>{
+    console.log(`Server is listening on port ${port}`);
+})
