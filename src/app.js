b1e15cb545b1ba1b85af906b34c3a3a4fbff7fcc
Vertika <mittalvertika1803200@gmail.com>
2021-06-22 04:57:01 +0530

    css added and code added for api

--- src/app.js
+++ src/app.js
@@ -5,6 +5,25 @@ const bodyParser=require("body-parser");
 const port=process.env.PORT||3000;
 const axios = require("axios");
 
+var cday = new Date();
+var ab = null;
+var cityName = null;
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -15,11 +34,29 @@ app.get("/",(req,res)=>{
 app.get("/about",(req,res)=>{
     res.render("about");
 })
-app.get("/weather",(req,res)=>{
-    res.render("weather");
-})
+app.get("/weather", (req, res) => {
+    const temp_api_key=process.env.temp_key
+    async function axiosTest() {
+      ab = await axios.get(
+        "http://api.openweathermap.org/data/2.5/weather?units=metric",
+        { params: { appid:temp_api_key,q: cityName } }
+      );
+      return ab;
+    }
+    axiosTest()
+      .then((data) => {
+        res.render("weather", {
+          location: cityName,
+          day: dayNames[cday.getDay()],
+          month: monthNames[cday.getMonth()],
+          date: cday.getDate() ,
+          currenttemp: data.data.main.temp,
+        });
+      })
+      .catch((err) => console.log(err));
+  });
 app.get("*",(req,res)=>{
-    res.send("404 error");
+    res.render("error");
 })
 app.listen(port,(req,res)=>{
     console.log(`Server is listening on port ${port}`);
